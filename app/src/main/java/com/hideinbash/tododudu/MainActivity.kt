24372848a935d7a8f72d90c663a5120999bd2e91
wslee6704 <felix6704@cau.ac.kr>
2025-06-04 02:28:35 +0900

    [Feat] : 바텀내비게이션뷰, 작업용 프래그먼트 틀 제작

--- app/src/main/java/com/hideinbash/tododudu/MainActivity.kt
+++ app/src/main/java/com/hideinbash/tododudu/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.hideinbash.tododudu
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.hideinbash.tododudu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+        if (savedInstanceState == null) {
+
+            replaceFragment(HomeFragment())
+            binding.bottomNavigationView.selectedItemId = R.id.nav_home
         }
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            Log.d("BottomNav", "Clicked item: ${item.itemId}") // ← 로그 추가
+            when (item.itemId) {
+                R.id.nav_mypage -> replaceFragment(MypageFragment())
+                R.id.nav_todo-> replaceFragment(TodoFragment()) // 습관
+                R.id.nav_home -> replaceFragment(HomeFragment()) // 홈
+
+            }
+            true
+        }
+
+    }
+    fun replaceFragment(fragment: Fragment, bundle: Bundle? =null) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.mainContainer.id, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }

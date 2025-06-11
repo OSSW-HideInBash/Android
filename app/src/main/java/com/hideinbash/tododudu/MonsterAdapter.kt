c7cebb8e75085a0c8792f77d8864495184542268
riadan710 <riadan710@gmail.com>
2025-06-11 23:14:16 +0900

    [Feat] 몬스터 GIF 띄우기

--- app/src/main/java/com/hideinbash/tododudu/MonsterAdapter.kt
+++ app/src/main/java/com/hideinbash/tododudu/MonsterAdapter.kt
@@ -4,8 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MonsterAdapter(
     private var items: List<Todo>,
@@ -20,13 +20,18 @@ class MonsterAdapter(
     override fun onBindViewHolder(holder: MonsterAdapter.MonsterViewHolder, position: Int) {
         val item = items[position]
 
-        val monsterRes = when(item.priority) {
-            1 -> R.drawable.monster_1
-            2 -> R.drawable.monster_2
-            3 -> R.drawable.monster_3
-            else -> R.drawable.monster_3 // 기본 몬스터 이미지
+        // 아이템의 우선순위에 따라 몬스터 이미지 glide 사용해서 url 가져와서 설정
+
+        val gifUrl = when(item.priority) {
+            1 -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif"
+            2 -> "https://animatedoss.s3.amazonaws.com/14563cf1-a95b-4110-a12c-4f0ee8f36fd8/video.gif"
+            3 -> "https://animatedoss.s3.amazonaws.com/da32ac1c-a684-49aa-9ef3-0a9ca1259612/video.gif"
+            else -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif" // 기본 몬스터 이미지 URL
         }
-        holder.monsterIv.setImageResource(monsterRes)
+        Glide.with(holder.itemView.context)
+            .asGif()
+            .load(gifUrl)
+            .into(holder.monsterIv)
 
         // 아이템 클릭 리스너 설정
         holder.monsterIv.setOnClickListener {

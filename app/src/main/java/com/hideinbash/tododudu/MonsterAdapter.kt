c8e0d40cb297582275d966dbeae08c8d6d10eeed
riadan710 <riadan710@gmail.com>
2025-06-12 15:37:24 +0900

    [Feat] 몬스터 처리 애니메이션 구현

--- app/src/main/java/com/hideinbash/tododudu/MonsterAdapter.kt
+++ app/src/main/java/com/hideinbash/tododudu/MonsterAdapter.kt
@@ -12,6 +12,9 @@ class MonsterAdapter(
     private val onItemClick: (Todo) -> Unit
 ) : RecyclerView.Adapter<MonsterAdapter.MonsterViewHolder>() {
 
+    // 완료 예정 상태인 아이템들의 ID를 저장
+    private val completingItems = mutableSetOf<Long>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonsterViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_monster, parent, false)
         return MonsterViewHolder(view)
@@ -20,22 +23,37 @@ class MonsterAdapter(
     override fun onBindViewHolder(holder: MonsterAdapter.MonsterViewHolder, position: Int) {
         val item = items[position]
 
-        // 아이템의 우선순위에 따라 몬스터 이미지 glide 사용해서 url 가져와서 설정
+        // 완료 예정 상태인지 확인
+        val isCompleting = completingItems.contains(item.id)
 
-        val gifUrl = when(item.priority) {
-            1 -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif"
-            2 -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif"
-            3 -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif"
-            else -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif" // 기본 몬스터 이미지 URL
+        val gifUrl = if (isCompleting) {
+            // 완료 상태 이미지 (priority별로 구분)
+            when(item.priority) {
+                1 -> "https://animatedoss.s3.amazonaws.com/fad01384-aeea-4539-946e-025387d43e81/video.gif"
+                2 -> "https://animatedoss.s3.amazonaws.com/fad01384-aeea-4539-946e-025387d43e81/video.gif"
+                3 -> "https://animatedoss.s3.amazonaws.com/fad01384-aeea-4539-946e-025387d43e81/video.gif"
+                else -> "https://animatedoss.s3.amazonaws.com/fad01384-aeea-4539-946e-025387d43e81/video.gif"
+            }
+        } else {
+            // 일반 상태 이미지 (priority별로 구분)
+            when(item.priority) {
+                1 -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif"
+                2 -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif"
+                3 -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif"
+                else -> "https://animatedoss.s3.amazonaws.com/59e26adf-ecd7-40e6-b319-1a5510dd42e6/video.gif"
+            }
         }
+
         Glide.with(holder.itemView.context)
             .asGif()
             .load(gifUrl)
             .into(holder.monsterIv)
 
-        // 아이템 클릭 리스너 설정
+        // 아이템 클릭 리스너 설정 (완료 예정 상태가 아닐 때만)
         holder.monsterIv.setOnClickListener {
-            onItemClick(item)
+            if (!isCompleting) {
+                onItemClick(item)
+            }
         }
     }
 
@@ -48,5 +66,20 @@ class MonsterAdapter(
     fun updateList(newItems: List<Todo>) {
         items = newItems
         notifyDataSetChanged()
+    }
+
+    // 특정 아이템을 완료 예정 상태로 설정
+    fun setItemCompleting(todoId: Long) {
+        completingItems.add(todoId)
+        // 해당 아이템의 위치를 찾아서 업데이트
+        val position = items.indexOfFirst { it.id == todoId }
+        if (position != -1) {
+            notifyItemChanged(position)
+        }
+    }
+
+    // 완료 예정 상태 해제 (실제 완료 후 목록에서 제거될 때)
+    fun removeCompletingItem(todoId: Long) {
+        completingItems.remove(todoId)
     }
 }
